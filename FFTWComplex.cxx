27db5fbbe068c09d161002cedbf4df8d6dd7ffe4
rjn <rjn@hep.ucl.ac.uk>
2010-01-25 12:44:35 +0000

    Adding division rule for FFTWComplex

--- FFTWComplex.cxx
+++ FFTWComplex.cxx
@@ -43,6 +43,22 @@ FFTWComplex& FFTWComplex::operator*=(const FFTWComplex &rhs)
   return *this;
 }
 
+
+FFTWComplex& FFTWComplex::operator/(const FFTWComplex &rhs)
+{
+  return FFTWComplex(*this) /= rhs;
+}
+
+FFTWComplex& FFTWComplex::operator/=(const FFTWComplex &rhs)
+{
+  FFTWComplex other(rhs);
+  Double_t mag=this->getAbs()/other.getAbs();
+  Double_t phase=this->getPhase()-other.getPhase();
+  this->setMagPhase(mag,phase);
+  return *this;
+}
+
+
 void FFTWComplex::setMagPhase(Double_t mag, Double_t phase) 
 {
   this->re=mag*TMath::Cos(phase);
