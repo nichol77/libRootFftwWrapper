7e8a8d9ec7c6b14fe95e6405d052e075b9e9ea90
MyoungchulK <kmc8907@gmail.com>
2022-05-21 00:31:11 -0500

    getPowers() function for SineSubtract class

--- include/SineSubtract.h
+++ include/SineSubtract.h
@@ -647,6 +647,11 @@ namespace FFTtools
         /* Return the number of traces fit for */
         int getNGraphs() const { return r.phases.size(); } 
 
+        /** Get array of the sequence of powers vs. iteration 
+         * lighter version of getPowerSequence()
+         **/
+        const double * getPowers() const {return &r.powers[0];}
+
         /** Get array of fit phases for the ith trace */
         const double * getPhases(int g) const { return &r.phases[g][0]; }
 
